89893f2f1a45dc589e8c5afda75501551e3660cb
isaac-scarrott <isaac.scarrott@dazn.com>
2023-02-26 17:02:34 +0000

    fullName -> name

--- apps/documentum/src/schema/evergreen-config.ts
+++ apps/documentum/src/schema/evergreen-config.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import PresetConfigSchema from "./presets/index.js";
 
 const EvergreenConfigSchema = z.object({
-  fullName: z.string().min(1),
+  name: z.string().min(1),
   description: z.string().min(1).optional(),
   generates: z.array(PresetConfigSchema),
 });
