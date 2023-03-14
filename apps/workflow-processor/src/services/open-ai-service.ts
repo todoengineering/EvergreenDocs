7d44d8540283492eab94dc9e79a0c88d6dd2152c
Isaac Scarrott <isaac.scarrott@dazn.com>
2023-03-14 16:15:38 +0000

    feat: chat gpt api and model (#65)

--- apps/workflow-processor/src/services/open-ai-service.ts
+++ apps/workflow-processor/src/services/open-ai-service.ts
@@ -11,9 +11,9 @@ const openai = new OpenAIApi(
 );
 
 const createCompletion = async (prompt: string) => {
-  const response = await openai.createCompletion({
+  const response = await openai.createChatCompletion({
     model: config.openAi.model,
-    prompt,
+    messages: [{ role: "user", content: prompt }],
     temperature: 0,
     max_tokens: 2048,
     top_p: 1,
@@ -21,14 +21,14 @@ const createCompletion = async (prompt: string) => {
     presence_penalty: 0,
   });
 
-  if (!response.data.choices[0].text) {
+  if (!response.data.choices[0].message?.content) {
     throw new Error("No generated text");
   }
 
   // TODO: Less logging here. Maybe we should save in dynamo or something
-  console.log(`Generated: ${response.data.choices[0].text}`);
+  console.log(`Generated: ${response.data.choices[0].message?.content}`);
 
-  return response.data.choices[0].text;
+  return response.data.choices[0].message?.content;
 };
 
 export { createCompletion };
