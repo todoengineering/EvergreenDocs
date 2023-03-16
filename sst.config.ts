2c06100f02ebb8b648e459f25dfd91a8ee0b336a
isaac-scarrott <isaac.scarrott@dazn.com>
2023-03-16 20:54:40 +0000

    remove cdk import

--- sst.config.ts
+++ sst.config.ts
@@ -11,7 +11,6 @@ import cacheStack from "./packages/stacks/src/cache.js";
 import githubActionsPermissionsStack from "./packages/stacks/src/github-actions-permissions.js";
 import route53Stack from "./packages/stacks/src/route53.js";
 import websiteStack from "./packages/stacks/src/website.js";
-import * as cdk from "aws-cdk-lib";
 
 const validStages = ["isaac-development", "dan-development", "production"];
 
